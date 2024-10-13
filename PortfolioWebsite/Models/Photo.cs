d618c79434f106ae88b7e0567ca1c7b3192406b5
Isaac Detert <detert7392@students.cptc.edu>
2024-10-13 16:35:36 -0700

    Created photo object, still need functionality to store a photo to display on screen.

---
+++ PortfolioWebsite/Models/Photo.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioWebsite.Models
+{
+    /// <summary>
+    /// Represents a single photo object
+    /// </summary>
+    public class Photo
+    {
+        /// <summary>
+        /// Uniquely identifies each photo in the database
+        /// </summary>
+        [Key]
+        public int PhotoId { get; set; }
+
+        /// <summary>
+        /// Description of a photo
+        /// </summary>
+        [Required]
+        public string PhotoDescription { get; set; }
+
+        /// <summary>
+        /// Name of a photo
+        /// </summary>
+        [Required]
+        public string PhotoName { get; set; }
+
+        // To Do: Add a way to actually contain a photo to display
+    }
+}
