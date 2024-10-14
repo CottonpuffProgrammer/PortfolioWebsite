4931e23e9e18ecd760cb7683e193bc9452e8f667
Isaac Detert <detert7392@students.cptc.edu>
2024-10-13 17:42:33 -0700

    Created local database for testing, can be adjusted as needed when new tables must be added.

---
+++ PortfolioWebsite/Models/PhotoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PortfolioWebsite.Models
+{
+    public class PhotoContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // Connection String
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Photolio;Trusted_Connection=True;");
+        }
+
+        public DbSet<Photo> photos { get; set; }
+    }
+}
