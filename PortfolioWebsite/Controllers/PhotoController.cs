85079a6c253e2ed55e761b94faba10ff7084ab76
Isaac Detert <detert7392@students.cptc.edu>
2024-11-10 16:38:57 -0800

    Added the ability to view all photos stored in wwwroot on the View Photos Page, added comments to complex chunks of code for better understanding.

--- PortfolioWebsite/Controllers/PhotoController.cs
+++ PortfolioWebsite/Controllers/PhotoController.cs
@@ -29,19 +29,29 @@ namespace PortfolioWebsite.Controllers
         [HttpPost]
         public IActionResult Upload(List<IFormFile> postedFiles)
         {
+            // Gets the necessary paths to add the files to the specified folder
             string wwwPath = this._environment.WebRootPath;
             string contentPath = this._environment.ContentRootPath;
 
+            // Creates a path to the images folder in wwwroot
             string path = Path.Combine(this._environment.WebRootPath, "images");
+
+            // If the image folder somehow doesn't exist, create it
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
             }
 
+            // Creates a list to temporarily track uploaded Files
             List<string> uploadedFiles = new List<string>();
+
+            // For each file posted on the Upload page
             foreach (IFormFile postedFile in postedFiles)
             {
+                // Creates the name of the current file
                 string fileName = Path.GetFileName(postedFile.FileName);
+
+                // Add the file to the image folder and return a success message
                 using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
                 {
                     postedFile.CopyTo(stream);
