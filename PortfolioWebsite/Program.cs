37545f723fe9faea3e33825d053f229d239c069d
Isaac Detert <detert7392@students.cptc.edu>
2024-11-26 16:29:24 -0800

    Test 2.

--- PortfolioWebsite/Program.cs
+++ PortfolioWebsite/Program.cs
@@ -5,9 +5,9 @@ using Photolio.Data;
 using PortfolioWebsite.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+//var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("DefaultConnection"));
 
 builder.Services.AddDefaultIdentity<PhotolioUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
